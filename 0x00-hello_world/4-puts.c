daa65f9d2bea1947daa22477e49cf00911522b38
Benkendo <benfransiswinkly48@gmail.com>
2022-12-11 06:35:33 -0800

    added first c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+	// this line below prints given string
+        char str1[] = "Programming is like building a multilingual puzzle";
+
+	//puts is used to display the outputs
+        puts(str1);
+
+
+	return 0;	
+	
+
+}

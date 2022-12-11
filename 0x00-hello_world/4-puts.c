b2e5e74c9c2b09901403ee1d1580305911d3b673
Benkendo <benfransiswinkly48@gmail.com>
2022-12-11 06:58:53 -0800

    modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
 int main(){
-   char str1[] = "Programming is like building a multilingual puzzle\n"
-   puts(str1);
-   return 0;	
+   
+	char str1[] = "Programming is like building a multilingual puzzle\n"
+        puts(str1);
+        return 0;	
 	
 
 }

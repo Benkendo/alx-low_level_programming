c036002f3a5be154e0e07a60170b9c133924ac8a
Benkendo <benfransiswinkly48@gmail.com>
2022-12-11 08:19:02 -0800

    new updated file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-		
-	char str[]="Programming is like building a multilingual puzzle";
-	puts("%s\n",str);			
-	return 0;
-
+int main(void)
+{ char str[] = "Programming is like building a multilingual puzzle";
+puts("%s \n", str);
+return 0;
 }

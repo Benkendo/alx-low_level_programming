3f9483f3104c84530b9c402660c750c2e9a26bcd
Benkendo <benfransiswinkly48@gmail.com>
2022-12-11 09:44:18 -0800

    corrected with betty format

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,15 @@
 /**
  * main - main function
  * char -this for character
+ * int main(void) - is amain function
+ * return(0) - returns everything after execution
+ *return(mai)n- returns main function result
+ *
+ * Return() -return function
  */
 int main(void)
 {
-     char line[] = "Programming is like building a multilingual puzzle";
-     puts(line);
-     return (0);
- 
+char line[] = "Programming is like building a multilingual puzzle";
+puts(line);
+return (0);
 }

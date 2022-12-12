a48a3b9828397cc2ea9fe430e706cc28e9681d13
Benkendo <benfransiswinkly48@gmail.com>
2022-12-12 05:21:27 -0800

    file added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 /**
-*main - Entry point 
-*Description: 'the program's description' 
-* Return: Always 0 (Success)
+*main-Entry_point
+*Description:-the-program's_description
+* Return:Always-0-(Success)
 */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**                                                                                                                                    
+*main - Entry point                                                                                                                
+*Description: 'the program description 
+* Return: Always 0 (Success)
+*/
+
+int main()
+{
+char str1[] = "\"with proper grammar, but the outcome is a piece of art,";
+printf(str1);
+return (0);
+}

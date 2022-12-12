8470b3998690a2c0829d6f49cb59fe0aed697386
Benkendo <benfransiswinkly48@gmail.com>
2022-12-12 05:31:13 -0800

    file 2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-/**                                                                                                                                    
-*main - Entry point                                                                                                                
-*Description: 'the program description 
+/**
+*main - Entry point
+*Description: 'the program description
 * Return: Always 0 (Success)
 */
-
-int main()
+int main(void)
 {
 char str1[] = "\"with proper grammar, but the outcome is a piece of art,";
 printf(str1);

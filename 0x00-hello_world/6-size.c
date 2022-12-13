ed228aa2935f31200949fa81e9ff056c0d982901
Benkendo <benfransiswinkly48@gmail.com>
2022-12-13 06:33:34 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 /**
- *main-explain_main
- *Return (0)-return-value
- *Description-strings_value
- */
+*main-Express_main
+*Description ->function
+*Return:Always-0-(Success)
+*/
 int main(void)
 {
-   char character_type;
-   int integer_type;
-   long int longinteger_type;
-   long long int lltype;
-   float float_type;
-
-   printf("Size of char: %ld byte(s)\n",sizeof(character_type));
-   printf("Size of int: %ld byte(s)\n",sizeof(integer_type));
-   printf("Size of long int: %ld byte(s)\n",sizeof(longinteger_type));
-   printf("Size of long long int: %ld byte(s)\n",sizeof(lltype));
-   printf("Size of float: %ld byte(s)\n",sizeof(float_type));
+char character_type;
+int integer_type;
+long longinteger_type;
+long long lltype;
+float float_type;
+printf("Size of char: %ld byte(s)\n", sizeof(character_type));
+printf("Size of int: %ld byte(s)\n", sizeof(integer_type));
+printf("Size of long int: %ld byte(s)\n", sizeof(longinteger_type));
+printf("Size of long long int: %ld byte(s)\n", sizeof(lltype));
+printf("Size of float: %ld byte(s)\n", sizeof(float_type));
 return (0);
 }
 

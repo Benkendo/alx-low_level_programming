67bc1d73f8ac547092ffe57e65d96720c8043601
Benkendo <benfransiswinkly48@gmail.com>
2022-12-13 06:39:25 -0800

    returned

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,11 +11,11 @@ int integer_type;
 long longinteger_type;
 long long lltype;
 float float_type;
-printf("Size of char: %ld byte(s)\n", sizeof(character_type));
-printf("Size of int: %ld byte(s)\n", sizeof(integer_type));
-printf("Size of long int: %ld byte(s)\n", sizeof(longinteger_type));
-printf("Size of long long int: %ld byte(s)\n", sizeof(lltype));
-printf("Size of float: %ld byte(s)\n", sizeof(float_type));
+printf("Size of a char: %ld byte(s)\n", sizeof(character_type));
+printf("Size of an int: %ld byte(s)\n", sizeof(integer_type));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longinteger_type));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lltype));
+printf("Size of a float: %ld byte(s)\n", sizeof(float_type));
 return (0);
 }
 

fb87ebe856d8ea76ce0b1c1cb9545e173badad13
Benkendo <benfransiswinkly48@gmail.com>
2022-12-13 20:34:04 -0800

    file updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)
+ main(void)
 {
 		int n;
 
@@ -12,16 +12,16 @@ int main(void)
 		
 		if (n>0)
 		{
-	        return "n is positive\n";
+	        return "is positive\n";
 		}
 		if else(n==0){
-		return "n is zero\n";
+		return "is zero\n";
 		}
 		if else (n<0){
-		return " n is negative\n";
+		return "is negative\n";
 		}
 		else
-		return "n is not valid number\n"
+		return "is not valid number\n"
 
 		return (0);
 
